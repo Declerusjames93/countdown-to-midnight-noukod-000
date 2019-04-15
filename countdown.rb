0a967cd0af69b92452837a922475624b758aeffe
James Declerus <james.declerus@esih.edu>
2019-04-15 12:20:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(entier)
+  counter = entier
+  while counter > 0
+puts "#{number} SECOND(S)!"
+    counter -= 1
+    return "HAPPY NEW YEAR!"
+  end
+
 end
